423a36c1e44e0c74d9e8de42e521baa95211b000
Bernardo Michels <bernardomichelsjob@gmail.com>
2024-09-18 21:27:38 -0300

    update

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './Header.css';
+import Button from '@mui/material/Button';
 
 function Header() {
   return (
     <header className="header">
-
+      <a href="https://twitter.com/Dave_Conner" class="btn btn-1">
+      <svg>
+        <rect x="0" y="0" fill="none" width="100%" height="100%"/>
+      </svg>
+     Entrar
+    </a>
     </header>
   );
 }
